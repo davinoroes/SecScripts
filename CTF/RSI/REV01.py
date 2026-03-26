dfa95ced503207cca97c603e2b1f8c7341ef7a7e
davinoroes <davinoroes1918@gmail.com>
2026-03-26 09:02:14 -0300

    Script REV01.bin

--- CTF/RSI/REV01.py
+++ CTF/RSI/REV01.py
@@ -34,6 +34,22 @@ for i in range(size):
 
 print("Senha :", password)
 
+"""In this lab, I had a .bin file that asked for a password to deliberate on the flag.
+   But the file gives some hints. First it says about the password length and after the
+   user gets it right, the file gives the first password index (char) which is incorrect.
+   For that, first i create a size_discover fuction, which process the archive and sends an ever-increasing password,
+   to discover len(real_password). After that, using the size_discover return, we permutate for each char,
+   until find the correct password.
+   """
+
+
+"""Neste laboratório, eu tinha um arquivo .bin que pedia uma senha para mostrar o sinalizador.
+ Mas o arquivo dá algumas dicas. Primeiro diz sobre o comprimento da senha e depois o o usuário acerta,
+ o arquivo fornece o primeiro índice de senha (char), que está incorreto.
+ Para isso, primeiro criei uma função size_discover, que processa o arquivo e envia uma senha cada vez maior, para descobrir len(real_password).
+ Depois disso, usando o retorno size_discover, permutamos para cada char , até encontrar a senha correta.
+"""
+
 
 
 
